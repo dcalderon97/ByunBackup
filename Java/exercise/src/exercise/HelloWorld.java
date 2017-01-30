d47244e4cc8bdf0dcf0b1d06d80f78de193b43fa
Daniel Calderon <danielcalderon@DanielsMacBookPro.attlocal.net>
2017-01-29 20:18:38 -0800

    Added to Byun backup

---
+++ Java/exercise/src/exercise/HelloWorld.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int answer;
+		System.out.println("Hello CST 338!");
+		System.out.println("This is a simple calculation");
+		answer = 2+2;
+		System.out.println("2 plus 2 is" + answer);
+	}
+
+}
